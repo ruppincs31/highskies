41f4a567a85ddff02984cd9622d5958ec90364dc
ruppincs31 <65138524+ruppincs31@users.noreply.github.com>
2020-08-21 18:54:19 +0300

    added ToursCompany class created sign up/in for companies to use

---
+++ WebApplication4/Models/TourCompany.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class TourCompany
+    {
+        string companyUserName;
+        string companyPassword;
+        string companyName;
+        string companyEmail;
+
+        public TourCompany(string companyUserName, string companyPassword, string companyName, string companyEmail)
+        {
+            this.companyUserName = companyUserName;
+            this.companyPassword = companyPassword;
+            this.companyName = companyName;
+            this.companyEmail = companyEmail;
+        }
+
+        public string CompanyUserName { get => companyUserName; set => companyUserName = value; }
+        public string CompanyPassword { get => companyPassword; set => companyPassword = value; }
+        public string CompanyName { get => companyName; set => companyName = value; }
+        public string CompanyEmail { get => companyEmail; set => companyEmail = value; }
+
+
+
+    }
+}
