6fa29fd9a757d972dddf915be6789e37c15aa881
ruppincs31 <ruppincs31@gmail.com>
2020-08-23 02:46:43 +0300

    swal for the tours offers added, ordercompletion done, next is logic for the tour according to the connections

--- WebApplication4/Global.asax.cs
+++ WebApplication4/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.SessionState;
 
 namespace WebApplication4
 {
@@ -18,6 +19,16 @@ namespace WebApplication4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        public override void Init()
+        {
+            this.PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init();
+        }
+
+        void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            System.Web.HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
         }
     }
 }
